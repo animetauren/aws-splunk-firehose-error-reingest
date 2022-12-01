68c1ef105de093913d40d4a552c598bb54177a5f
Henry Robalino <animetauren@users.noreply.github.com>
2022-12-01 10:07:32 -0500

    Removed test fn

--- firehose-reingest/lambda_function.py
+++ firehose-reingest/lambda_function.py
@@ -51,6 +51,7 @@ def lambda_handler(event, context):
         destS3=0
         s3payload={}
         reingest_count=1
+        
         for line in text.split("\n"): #process every 'batch'
             dest='FH' #default destination will be FH
             if len(line)>0:
@@ -144,19 +145,7 @@ def lambda_handler(event, context):
         
     except Exception as e:
         print(e)
-        raise e
-    
-def test_event(message):
-    #if the event has had some processing, it may have additional json wrapper. The RAW event should be contained in "event"
-    #this function will attempt to extract that raw event field - if there is an error, i.e. the content isn't a json, it will return the message as is
-    try:
-        data=json.loads(message)
-        event=data['event']
-        return json.dumps(event)
-    
-    except:
-        return message
-        
+        raise e    
 
 def putRecordsToFirehoseStream(streamName, records, client, attemptsMade, maxAttempts):
     failedRecords = []
